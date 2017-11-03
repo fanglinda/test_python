c7269b0d235054d95cdae25db20b21723326c946
emaayan <fanglinda@hotmail.com>
2017-11-02 21:28:12 -0400

    Added 2nd line

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
 print("Hello world!")
-print("From Git!")
-print("this one should be automatically triggerd!")
+# This is the complete for loop program from page 32
+
+for x in range(0, 10):
+    print(x)
+
